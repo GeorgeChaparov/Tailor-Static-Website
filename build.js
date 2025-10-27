87dd9ea87167dec7e58daa6f6fe6e61fa670dc03
GeorgeChaparov <GeorgeChaparov@gmail.com>
2025-10-27 17:57:21 +0200

    Reworking the html structure to match the new design.

--- build.js
+++ build.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const reviews = require('./reviews.json');
+const QnA = require("./QnA.json");
 
-const schema = {
+const reviewSchema = {
   "@context": "https://schema.org",
   "@type": "LocalBusiness",
   "name": "Ателието на Ани",
@@ -11,45 +12,79 @@ const schema = {
   "email": "info@eleganttailor.com",
   "address": {
     "@type": "PostalAddress",
-    "streetAddress": "123 Main Street",
-    "addressLocality": "Your City",
-    "postalCode": "12345",
+    "streetAddress": "ж.к. Възраждане, ул. „Доростол“ 73",
+    "addressLocality": "Ruse",
+    "postalCode": "7002",
     "addressCountry": "BG"
   },
   "telephone": "+359-123-456-789",
   "geo": {
     "@type": "GeoCoordinates",
-    "latitude": "43.8356",
-    "longitude": "25.9657"
+    "latitude": "43.8601081",
+    "longitude": "25.9738515"
   },
   "openingHours": "Mo-Fr 09:00-18:00",
   "url": "https://example.com",
   "sameAs": [
     "https://www.facebook.com/yourtailorpage"
   ],
-  "review": reviews.map(r => ({
+  "review": reviews.map(review => ({
     "@type": "Review",
     "author": { 
         "@type": "Person", 
-        "name": r.author 
+        "name": review.author 
     },
-    "datePublished": r.date,
-    "reviewBody": r.text
+    "datePublished": review.date,
+    "reviewBody": review.text
+  }))
+};
+
+const QnASchema = {
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "mainEntity": QnA.map((qna) => ({
+    "@type": "Question",
+    "name": qna.Question,
+    "acceptedAnswer": {
+      "@type": "Answer",
+      "text": qna.Answer
+    }
   }))
 };
 
 // Convert to JSON-LD string
-const schemaHtml = `<script type="application/ld+json">
-  {
-    ${JSON.stringify(schema, null, 2)}
-  }
+const reviewSchemaHtml = `
+<script type="application/ld+json">
+  ${JSON.stringify(reviewSchema, null, 2)}
 </script>`;
+
+const qnaSchemaHtml = `
+<script type="application/ld+json">
+  ${JSON.stringify(QnASchema, null, 2)}
+</script>`;
+
+const reviewsHtml = `${reviews.map((review, index)=>{
+return  `
+<article class="review">
+  <footer>
+    <address><a href='${review.link}'>Link to the review source</a></address>
+    <img class="userImage" src='./images/${index}' alt="Профилната снимка, в facebook, на потребителя, който е публикувал този коментар.">
+    <h3 class="userName">${review.author}</h3>
+    <time class="reviewPublicationDate" datetime=${review.date}>${review.date}</time>
+  </footer>
+    <div>
+        <p class="reviewText">${review.text}</p>
+    </div>
+</article>`
+})}`;
 
 // Read your HTML template
 let html = fs.readFileSync('./src/index.html', 'utf-8');
 
 // Insert the schema in <head> (or wherever appropriate)
-html = html.replace('<!-- REVIEW_SCHEMA_PLACEHOLDER -->', schemaHtml);
+html = html.replace('<!-- REVIEW_SCHEMA_PLACEHOLDER -->', reviewSchemaHtml);
+html = html.replace('<!-- QnA_SCHEMA_PLACEHOLDER -->', qnaSchemaHtml);
+html = html.replace('<!-- REVIEWS_PLACEHOLDER -->', reviewsHtml);
 
 // Write final HTML
 fs.writeFileSync('./dist/index.html', html);
